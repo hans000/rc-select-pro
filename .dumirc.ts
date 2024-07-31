8f52731b5bd015d41bbc139830e0b68341bb5cfd
hans000 <hhance@163.com>
2024-07-31 17:06:10 +0800

    Update .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   base: '/rc-select-pro/',
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/rc-select-pro' : '/',
   runtimePublicPath: {},
   themeConfig: {
     name: 'Select',
